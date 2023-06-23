c4836d7f647385334396470c788205a4c28935f8
Teresa Chen <qianyingteresachen@gmail.com>
2023-06-23 16:51:47 -0400

    updated seurity config

--- src/main/java/com/skillstorm/spyglass/config/SecurityConfig.java
+++ src/main/java/com/skillstorm/spyglass/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.skillstorm.spyglass.config;
 import java.util.Arrays;
 
 import org.springframework.context.annotation.Bean;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,27 +16,26 @@ import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 public class SecurityConfig {
 
 	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-			  .anyRequest().authenticated()
-			  .and()
-			  .csrf().disable()
-			  .oauth2Login(); 
-		
-		http.cors().configurationSource(request -> {
-			CorsConfiguration corsConfig = new CorsConfiguration();
-			corsConfig.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
-			corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
-			corsConfig.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
-			corsConfig.setAllowCredentials(true); 
-			corsConfig.setMaxAge(3600L); // For an hour, cache the preflight cors request 
-			
-			
-			UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-			source.registerCorsConfiguration("/**", corsConfig);
-			
-			return corsConfig;
-		});
+
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(requests -> requests
+                .anyRequest().authenticated())
+                .csrf(csrf -> csrf.disable())
+                .oauth2Login(withDefaults());
+        http.cors(cors -> cors.configurationSource(request -> {
+            CorsConfiguration corsConfig = new CorsConfiguration();
+            corsConfig.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+            corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+            corsConfig.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+            corsConfig.setAllowCredentials(true);
+            corsConfig.setMaxAge(3600L); // For an hour, cache the preflight cors request 
+            
+            
+            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+            source.registerCorsConfiguration("/**", corsConfig);
+
+            return corsConfig;
+        }));
 		
 		return http.build();
 	}
--- src/main/java/com/skillstorm/spyglass/controllers/GoalController.java
+++ src/main/java/com/skillstorm/spyglass/controllers/GoalController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import com.skillstorm.spyglass.services.GoalService;
 
 @RestController
 @RequestMapping("/goals")
+@CrossOrigin(allowCredentials = "true", originPatterns = "http://localhost:5173")
 public class GoalController {
 	
 	@Autowired
